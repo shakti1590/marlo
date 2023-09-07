f05d31c918b27d9830c51ffbc68cdb7fbcc50b85
shakti singh <60788912+shakti1590@users.noreply.github.com>
2023-09-07 16:08:19 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -9,11 +9,9 @@ def register():
     data = request.get_json()
     first_name = data.get('first_name')
     last_name = data.get('last_name')
-    # Process the registration data and add user to the list
     new_user = {
         'first_name': first_name,
         'last_name': last_name,
-        # ... other user details ...
     }
     users.append(new_user)
     return jsonify({'message': 'Registration successful'})
