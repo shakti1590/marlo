98fd2582521ee0eb134d6a84d62e3438d1388692
shakti singh <60788912+shakti1590@users.noreply.github.com>
2023-09-07 16:09:10 +0530

    Update product_upload.py

--- product_upload.py
+++ product_upload.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/upload_products', methods=['POST'])
 def upload_products():
-    # Your product upload API code here
     uploaded_file = request.files['file']
-    # Process the uploaded file
     return jsonify({'message': 'Product upload successful'})
 
 if __name__ == '__main__':
