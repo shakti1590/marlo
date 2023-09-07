1097ecee1e4e352d53035a91b66c36fafa839ed9
shakti singh <60788912+shakti1590@users.noreply.github.com>
2023-09-07 16:03:16 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Sample user data (replace this with your actual user data)
+users = [
+    {"username": "user1", "password": "password1"},
+    {"username": "user2", "password": "password2"},
+    # ... other users ...
+]
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            return jsonify({'message': 'Login successful'})
+    return jsonify({'message': 'Invalid credentials'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
